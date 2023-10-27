55371e2bf2ff603fdf907bdd2ec7cc29aefb4e98
mohamed-slama <75334129+mohamed-slama@users.noreply.github.com>
2023-10-27 11:39:21 +0100

    sqd

--- server.js
+++ server.js
@@ -1,34 +1,32 @@
-import express from "express"
-import dotenv from "dotenv"
-import bodyParser from "body-parser"
-import mongoose from "mongoose"
-import auth from "./routes/AuthentificationRoute.js" 
-import play from "./routes/player.js"
-import serv from "./routes/serveritems.js"
-const app = express()
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import auth from "./routes/AuthentificationRoute.js";
+import play from "./routes/player.js";
+import serv from "./routes/serveritems.js";
+const app = express();
 
-app.use(bodyParser.urlencoded({extended : false}))
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-mongoose.set('strictQuery', true);
+mongoose.set("strictQuery", true);
 
 //setting the database
-dotenv.config() ; 
+dotenv.config();
 export const connect = async () => {
-    try {
+  try {
     await mongoose.connect(process.env.MONGO);
-    
-    } catch (error) {
-        throw error
-    }
-   
-}
- 
+  } catch (error) {
+    throw error;
+  }
+};
 
-app.use(auth)
-app.use(play)
-app.use(serv)
+app.use("/", "test valide");
+app.use(auth);
+app.use(play);
+app.use(serv);
 
 app.listen(13756, () => {
-    connect() ; 
-    console.log("listening on " + 13756);
-})
+  connect();
+  console.log("listening on " + 13756);
+});
